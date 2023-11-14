b6f777f34e7ef3319921adb48b3dad77eaaa95ca
Kylian Janssens <kylianjanssens@hotmail.com>
2023-11-14 19:56:53 +0100

    removing the app

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,10 @@ class ProjectsController < ApplicationController
   def index
   end
 
+  def show
+    @projects = Project.New
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'projects#index'
-
+  resources :projects
   get '/about', to: 'pages#about', as: 'about'
   get '/shop', to: 'pages#shop', as: 'shop'
   get '/contact', to: 'pages#contact', as: 'contact'
